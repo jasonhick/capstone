5564c8d4329ae705fe95716d72177b593f078467
Jason Hick <1746397+jasonhick@users.noreply.github.com>
2025-01-14 17:34:16 +0000

    Feature: Added Cors and Postman collection

--- apps/backend/src/__init__.py
+++ apps/backend/src/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 
 from .config import Config
 from .database import db, init_db
@@ -9,6 +10,18 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     app.url_map.strict_slashes = False  # This disables the automatic redirect
+
+    # Enable CORS
+    CORS(
+        app,
+        resources={
+            r"/api/*": {
+                "origins": ["http://127.0.0.1:3000"],
+                "methods": ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+                "allow_headers": ["Content-Type", "Authorization"],
+            }
+        },
+    )
 
     # Initialize database and models (this will also init the app)
     init_db(app)
